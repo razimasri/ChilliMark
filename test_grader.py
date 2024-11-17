a28c8cce81f77e74b02e2e22bb910ca232245436
razimasri <46912821+razimasri@users.noreply.github.com>
2024-11-17 10:49:45 +0700

    uses empty bubble to ensure contour consistency and add clean markup

--- test_grader.py
+++ test_grader.py
@@ -25,8 +25,9 @@ def crop_area(image, instructions="Select Area",blur=False):
         width = 1200
     
     temp =cv2.resize(temp, (width,height)) 
+    cv2.putText(temp,instructions, (50,75),cv2.FONT_HERSHEY_TRIPLEX, 1,(255, 255, 255),10,lineType=cv2.LINE_AA) 
     cv2.putText(temp,instructions, (50,75),cv2.FONT_HERSHEY_TRIPLEX, 1,(1, 1, 1),2,lineType=cv2.LINE_AA) 
-    cv2.putText(temp,instructions, (50,75),cv2.FONT_HERSHEY_TRIPLEX, 1,(100, 156, 245),1,lineType=cv2.LINE_AA) 
+    cv2.putText(temp,instructions, (50,75),cv2.FONT_HERSHEY_TRIPLEX, 1,(200, 10, 145),1,lineType=cv2.LINE_AA) 
     
     x,y,w,h = cv2.selectROI(instructions, temp, fromCenter=False,showCrosshair=True)
     x= int((x-5)*scale) 
@@ -56,7 +57,6 @@ def check_fill(contour,thresh,index):
 
 def manual_bubble(q_area):
 	
-
 	q_box = crop_area(q_area[0:300,0:400],"Select one EMPTY Bubble")
 
 	cnts,hier = get_contour(q_box,cv2.RETR_EXTERNAL)
@@ -69,7 +69,15 @@ def manual_bubble(q_area):
 	(_, _, w, h) = cv2.boundingRect(bub)
 	bub_hw = [h,w]
 	bub_fill = check_fill(cnts,get_thresh_image(q_box),-1)
-	return bub_hw, bub_fill
+	
+	bub -= contour_center(bub)
+	return bub_hw, bub_fill, bub
+
+def	contour_center(cnt):
+	M = cv2.moments(cnt)
+	cx = int(M['m10']/M['m00'])
+	cy = int(M['m01']/M['m00'])
+	return [cx,cy]
 
 def find_bubbles(q_area):
 	"""Goes through contour and returns List of only those of similar size to user defined bubble"""
@@ -82,7 +90,9 @@ def find_bubbles(q_area):
 		c=cv2.approxPolyDP(c,peri*0.02,True) #approx polly N is not in latest. will need to build from source
 		(_, _, w, h) = cv2.boundingRect(c)
 		if bub_hw[1]*0.8<= w <= bub_hw[1]*1.3 and bub_hw[0]*0.9 <= h <= bub_hw[0]*1.3 and hier[0][i][3]==-1: #q_ratio*0.9 <= ar <= q_ratio*1.1 and 
-			bubbles.append(c)	
+			
+			#This maps the default empty bubble onto all
+			bubbles.append(bub + contour_center(c))	
 
 	return bubbles
 
@@ -94,28 +104,45 @@ def sort_into_columns(bubbles):
 	columns = [[]]
 	bubbles,_ = imutils.contours.sort_contours(bubbles, method="left-to-right")
 	prev_x=0
+	jump = 1000
 	col_index = 0
 	temp_image = q_area.copy()
-
+	count=[1]
 	for i, bubble in enumerate(bubbles):	 
 		x,y,w,h= cv2.boundingRect(bubble)
 		
 		if abs(x-prev_x) > w//2 and i>0:
 			col_index +=1
 			columns.append([])
+			if abs(prev_x-x)/jump > 1.1:
+				count.append(0)  
+			count[-1] += 1
+			
+			jump = x-prev_x
 		
 		columns[col_index].append(bubble)
+
 		prev_x = x
-		colour_index = (col_index%choices) 
-		temp_image = cv2.drawContours(temp_image, bubbles, i, colours[colour_index], 3)
+		
+	for item in count:
+		if item == count[0]:
+			choices = count[0]
+		else:
+			return print("Detecting inconsistent choice number")
+
 
 	for i, column in enumerate(columns):
 		columns[i],_ = imutils.contours.sort_contours(column, method="top-to-bottom")
+		colour_index = (i%choices) 
+		cv2.drawContours(temp_image, columns[i], -1, colours[colour_index], 3)
+		
+		
+
 
 	show_image(q_area,temp_image)
-	return columns
+	return columns, choices
 
-def find_questions(columns):
+def find_questions(columns,choices):
 	"""Goes through the columns to buid sets of contours based on use define number of choices"""
     
 	temp_image = q_area.copy()
@@ -134,7 +161,7 @@ def find_questions(columns):
 		c+=choices
 	
 	for q, question in enumerate(questions):
-		temp_image = cv2.drawContours(temp_image, question, -1, colours[q%6], 3)
+		cv2.drawContours(temp_image, question, -1, colours[q%6], 3)
 	
 	show_image(q_area,temp_image)
 	return questions
@@ -151,11 +178,9 @@ def find_answers(q_area,questions,bub_fill):
 			if fill < bub_fill*1.3:
 				fill = 0
 			else:
-				temp_image = cv2.drawContours(temp_image, question, b, colours[0], 2)
-				
+				#temp_image = add_markup(colours[0],bubble,key.get(b),temp_image)
+				temp_image = cv2.drawContours(temp_image, question, b, colours[0], 3)	
 			answer.append(fill)
-		
-		
 		
 		max_fill = max(answer)
 		if not max_fill:
@@ -164,25 +189,43 @@ def find_answers(q_area,questions,bub_fill):
 			answers.append("Unclear")
 		else:answers.append(answer.index(max(answer)))
 		
-		temp_image = cv2.drawContours(temp_image, question, ANSWER_KEY.get(q), colours[1], 3)		
-		show_image(q_area,temp_image)
-		print(ANSWER_KEY.get(q))
-
-	st_answers=answers[:]
+		if ANSWER_KEY.get(q) != None:
+			temp_image = add_markup(colours[1],question[ANSWER_KEY.get(q)],key.get(ANSWER_KEY.get(q)),temp_image)
+			temp_image = cv2.drawContours(temp_image, question, ANSWER_KEY.get(q), colours[1], 3)		
+	show_image(q_area,temp_image)
+		
+	let_answers=answers[:]
 
 	for a, answer in enumerate(answers):
-		st_answers[a] = key.get(answer)
+		let_answers[a] = key.get(answer)
+
+	return answers, let_answers, temp_image
+
+def add_markup(colour,contour,choice,image):
+	x,y,w,h = cv2.boundingRect(contour)
+	textsize = cv2.getTextSize(choice,cv2.FONT_HERSHEY_SIMPLEX, 3, 3)[0]
 
 
-	return st_answers
+	
+	if textsize [1]>h:
+		text_y = y - h//2	
+		font_size=1.5
+		textsize = cv2.getTextSize(choice,cv2.FONT_HERSHEY_SIMPLEX, font_size, 3)[0]
+	else:
+		text_y = y+h//2+textsize[1]//2
+		font_size=3
+	
+	text_x = x+w//2-textsize[0]//2
+	cv2.putText(image,choice, (text_x,text_y),cv2.FONT_HERSHEY_SIMPLEX, font_size,(255,255,255),15,lineType=cv2.LINE_AA) 
+	cv2.putText(image,choice, (text_x,text_y),cv2.FONT_HERSHEY_SIMPLEX, font_size,colour,3,lineType=cv2.LINE_AA) 
+	return image
 
-def get_score(st_answer,ANSWER_KEY):
+def get_score(let_ans,ANSWER_KEY):
 	score = 0
-	for a, answer in enumerate(st_answers):
+	for a, answer in enumerate(let_ans):
 		if answer == ANSWER_KEY.get(a):
 			score+=1
 	return score
-
 
 def show_image(original,modified):
 	pyplot.subplot(121),pyplot.imshow(original,cmap = 'gray')
@@ -194,28 +237,22 @@ def show_image(original,modified):
 key = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F", "Unclear":"Unclear"}
 ANSWER_KEY = {0: 0, 1: 1, 2: 1, 3: 2, 4:1}
 
-choices = 4
+#choices = 4
 colours = [(200,0,0),(0,200,0),(0,0,200),(220,200,0),(200,0,200),(0,200,200)] #this can be related to binary 1-6 backwards
 
 # load the image, and define key areas
 filename = askopenfilename()  #need to add pdf and multiple images option
 image = cv2.imread(filename)
 q_area = crop_area(image.copy(),"Select question area",True)
-bub_hw, bub_fill = manual_bubble(q_area) 
+bub_hw, bub_fill, bub = manual_bubble(q_area) 
 bubbles = find_bubbles(q_area)
-columns = sort_into_columns(bubbles)
-questions = find_questions(columns)
-st_answers = find_answers(q_area,questions,bub_fill)
+columns,choices = sort_into_columns(bubbles)	#if jump//jump==1 count, if all counts agree, set as choices. otherwise prompt to ask?
+questions = find_questions(columns,choices)
+num_ans, let_ans, marked_img = find_answers(q_area,questions,bub_fill)
 
-#marked(st_answers)
+score = get_score(num_ans,ANSWER_KEY)
 
-
-
-
-score = get_score(st_answers,ANSWER_KEY)
-
-
-print(st_answers)
+print(let_ans)
 print(score)
 
     
