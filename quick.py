fb5694f930365d830d8a07ede44e5fb38d42cba9
razimasri <alrazimasri@gmail.com>
2024-11-18 15:08:57 +0700

    playing with pymupdf in quick file

--- quick.py
+++ quick.py
@@ -1,2 +1,12 @@
-for i in "ABCDEFGHI":
-    print(ord(i)-65)
+import tkinter.filedialog
+import pymupdf
+
+filename = tkinter.filedialog.askopenfilename()
+doc = pymupdf.open(filename)
+
+scans = []
+for i, page in enumerate(doc):
+    scans.append(page.get_pixmap())
+    mode = "RGBA" if scanspix.alpha else "RGB"
+    img = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
+    tkimg = ImageTk.PhotoImage(img)
